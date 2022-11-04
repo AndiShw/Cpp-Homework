e6b91b0e8d069d971e5cd90ff05e33f0da1a7af8
AndiShw <andreas-franz.schwab@stud.uni-bamberg.de>
2022-11-04 16:36:21 +0100

    First try of fibonacci

--- Sheet02/exercise2/simplevm/simplevm.cpp
+++ Sheet02/exercise2/simplevm/simplevm.cpp
@@ -12,7 +12,19 @@ int32_t runVM()
 void fibonacciProgram(unsigned n)
 // Print a VM program that calculates the nth fibonacci number.
 {
-    // Add your code here
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
+
+    // displays the first two terms which is always 0 and 1
+    std::cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
+
+    nextTerm = t1 + t2;
+
+    while (nextTerm <= n) {
+        std::cout << nextTerm << ", ";
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
 }
 //---------------------------------------------------------------------------
 } // namespace simplevm
